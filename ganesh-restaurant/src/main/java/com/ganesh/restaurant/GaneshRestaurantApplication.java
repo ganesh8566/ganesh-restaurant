9c659d65c99d4cde71b2c6a162ef74eaacf7bae4
ganesh8566 <ganeshnagam8566@gmail.com>
2025-07-01 11:46:04 +0530

    Initial Commit

---
+++ ganesh-restaurant/src/main/java/com/ganesh/restaurant/GaneshRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaneshRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaneshRestaurantApplication.class, args);
+	}
+
+}
